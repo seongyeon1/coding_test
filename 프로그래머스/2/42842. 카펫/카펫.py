8e86652cdd4f6ef129a25d9b08589f33c8605e18
seongyeon1 <83098550+seongyeon1@users.noreply.github.com>
2024-04-04 17:49:44 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,6 +1,18 @@
 def solution(brown, yellow):
-    x = (brown + 4 + pow(brown**2 - 8*brown - 16*yellow + 16, 0.5)) / 4
-    y = (brown + 4 - pow(brown**2 - 8*brown - 16*yellow + 16, 0.5)) / 4
-    answer = [x, y]
+    grid = brown + yellow
+    for n in range(3, int(grid ** 0.5) + 1): # 최소 길이부터 정사각형까지
+        if grid % n != 0:
+            continue
+        m = grid // n
+        if (n-2) * (m-2) == yellow:
+            return [m, n]
+
+
+
+
+# def solution(brown, yellow):
+#     x = (brown + 4 + pow(brown**2 - 8*brown - 16*yellow + 16, 0.5)) / 4
+#     y = (brown + 4 - pow(brown**2 - 8*brown - 16*yellow + 16, 0.5)) / 4
+#     answer = [x, y]
     
-    return answer
+#     return answer
