1dd6e0c44bfc5a3c3c0e4d7b73c4abf0229c955b
seongyeon1 <83098550+seongyeon1@users.noreply.github.com>
2024-04-09 12:19:38 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,10 +1,8 @@
 def solution(citations):
-    citations.sort()
     l = len(citations)
     while l >= 0:
         # h번 이상 인용된 논문
         t = len([c for c in citations if c >= l])
-        
         if t >= l:
             answer = l
             break
