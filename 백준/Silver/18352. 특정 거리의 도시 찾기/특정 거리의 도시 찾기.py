cf452f887ef0181e7a8851b6d8ca0b4e005e4d0e
seongyeon1 <83098550+seongyeon1@users.noreply.github.com>
2024-05-12 14:22:55 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1288 ms, Memory: 158864 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,6 +1,4 @@
 from collections import deque
-import sys
-input = sys.stdin.readline
 
 # N, M을 공백으로 구분하여 입력받기
 n, m, k, x = map(int, input().split())
@@ -10,21 +8,19 @@ for i in range(m):
     start, end = map(int, input().split())
     graph[start].append(end)
 
-# 모든 도시에 대한 최단 거리 초기화
 distance = [-1] * (n+1)
-distance[x] = 0 # 출발 도시까지의 거리는 0으로 설정
+distance[x] = 0 # 출발 도시의 거리는 0으로 설정
 
-# 너비 우선 탐색(BFS) 수행
 q = deque([x])
+cnt = 0
 while q:
-    now = q.popleft()
-    # 현재 도시에서 이동 할 수 있는 모든 도시를 확인
-    for next_node in graph[now]:
-        # 아직 방문하지 않은 도시라면
-        if distance[next_node] == -1:
-            # 최단 거리 갱신
-            distance[next_node] = distance[now] + 1
-            q.append(next_node)
+    v = q.popleft()
+
+    for i in graph[v]:
+        if distance[i] == -1:
+            distance[i] = distance[v] + 1
+            q.append(i)
+
 
 # 최단 거리가 K인 모든 도시의 번호를 오름차순으로 출력
 check = False
